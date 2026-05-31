b203b7cac5e84395a387a195a8d66cfa59e85ecf
Suvajit Patra <suvajit790@gmail.com>
2026-06-01 00:23:11 +0530

    TA details and Edu details added

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const themeToggle = document.getElementById('themeToggle');
-const icon = themeToggle.querySelector('i');
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('themeToggle');
+    const icon = themeToggle.querySelector('i');
 
-// Check for local storage preference
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme === 'dark') {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    icon.classList.replace('fa-moon', 'fa-sun');
-}
-
-themeToggle.addEventListener('click', () => {
-    let theme = 'light';
-    if (document.documentElement.getAttribute('data-theme') !== 'dark') {
+    // Assess local storage logic for theme caching
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
         document.documentElement.setAttribute('data-theme', 'dark');
         icon.classList.replace('fa-moon', 'fa-sun');
-        theme = 'dark';
-    } else {
-        document.documentElement.removeAttribute('data-theme');
-        icon.classList.replace('fa-sun', 'fa-moon');
     }
-    localStorage.setItem('theme', theme);
+
+    // Toggle execution event listener
+    themeToggle.addEventListener('click', () => {
+        let theme = 'light';
+        if (document.documentElement.getAttribute('data-theme') !== 'dark') {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            icon.classList.replace('fa-moon', 'fa-sun');
+            theme = 'dark';
+        } else {
+            document.documentElement.removeAttribute('data-theme');
+            icon.classList.replace('fa-sun', 'fa-moon');
+        }
+        localStorage.setItem('theme', theme);
+    });
 });
